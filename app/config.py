30518a266090b4be5522b02e477ade947b70c4e8
kumarswengineer <72960450+kumarswengineer@users.noreply.github.com>
2025-09-11 17:05:44 +0000

    fix: multi lang processing

--- app/config.py
+++ app/config.py
@@ -3,4 +3,6 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-OCR_LANG = os.getenv("OCR_LANG", "eng")
+OCR_LANG = "eng"
+OCR_LANG_2 = "ara"
+
--- app/services/ocr_service.py
+++ app/services/ocr_service.py
@@ -3,6 +3,7 @@ from PIL import Image
 import io
 from pdf2image import convert_from_bytes
 from app.config import OCR_LANG
+from app.config import OCR_LANG_2
 
 def extract_text(file_bytes: bytes, content_type: str) -> str:
     if content_type == "application/pdf":
@@ -10,7 +11,11 @@ def extract_text(file_bytes: bytes, content_type: str) -> str:
         text = ""
         for img in images:
             text += pytesseract.image_to_string(img, lang=OCR_LANG)
+            text += pytesseract.image_to_string(img, lang=OCR_LANG_2)
         return text
     else:
         image = Image.open(io.BytesIO(file_bytes))
-        return pytesseract.image_to_string(image, lang=OCR_LANG)
+        text = ""
+        text += pytesseract.image_to_string(image, lang=OCR_LANG)
+        text += pytesseract.image_to_string(image, lang=OCR_LANG_2)
+        return text
